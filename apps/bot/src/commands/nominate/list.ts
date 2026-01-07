c20db439ee896cd5ba50a7b9ad5aa7d05714f7ce
Kevin Weaver <kevinweaver2@gmail.com>
2026-01-06 23:11:29 -0800

    nominate list logs

--- apps/bot/src/commands/nominate/list.ts
+++ apps/bot/src/commands/nominate/list.ts
@@ -1,14 +1,28 @@
 import type { ChatInputCommandInteraction } from 'discord.js';
+import pino from 'pino';
 import { NomineeDisplayUtils } from '../../lib/nomineeDisplayUtils.js';
 import { CommandUtils } from '../../lib/commandUtils.js';
 import { ConfigService } from '../../lib/configService.js';
 
+const logger = pino();
 
 export async function handleListCommand(interaction: ChatInputCommandInteraction): Promise<void> {
   try {
     const guildId = ConfigService.getGuildId();
     
+    logger.info({
+      userId: interaction.user.id,
+      username: interaction.user.username,
+      guildId,
+      channelId: interaction.channelId
+    }, 'User requested nomination list');
+    
     const nominees = await NomineeDisplayUtils.getNomineesInQueueOrder(guildId);
+
+    logger.debug({
+      nomineeCount: nominees.length,
+      nominees: nominees.map(n => ({ name: n.name, state: n.state }))
+    }, 'Retrieved nomination queue');
 
     const queueEmbed = await NomineeDisplayUtils.createQueueEmbed(nominees);
     await interaction.reply({
